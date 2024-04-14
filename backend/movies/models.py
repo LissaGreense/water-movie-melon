b5c582d4832e2a55ccdc43389cb5d7a35222ad64
Ryfczynska Sara <sara.ryfczynska@nokia.com>
2024-04-14 22:26:16 +0200

    Add users with avatar and add rating

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Movie(models.Model):
@@ -7,3 +8,21 @@ class Movie(models.Model):
     user = models.CharField(max_length=20)
     date_added = models.DateTimeField('date_added')
     genre = models.CharField(max_length=20)
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Rate(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['movie', 'user'], name='unique_movie_user_rating')
+        ]
