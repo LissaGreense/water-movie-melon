3a00c9ec7bafff10c5655c96a410db724c6d8826
adlbrtbsr <creeper1739@gmail.com>
2024-04-14 22:50:46 +0200

    Implemented newMovieNight and expanded Movie database

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -7,3 +7,11 @@ class Movie(models.Model):
     user = models.CharField(max_length=20)
     date_added = models.DateTimeField('date_added')
     genre = models.CharField(max_length=20)
+    cover_link = models.CharField(max_length=150, default='')
+    duration = models.IntegerField(default=1)
+
+
+class MovieNight(models.Model):
+    host = models.CharField(max_length=50)
+    night_date = models.DateTimeField('night_date')
+    location = models.CharField(max_length=100)
--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("newNight", views.new_night, name='newNight'),
 ]
