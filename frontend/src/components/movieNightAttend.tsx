39736257d4b5c8eddd5a379612a399a9c5aa007c
adlbrtbsr <creeper1739@gmail.com>
2024-07-04 19:13:06 +0200

    changed == to === in length comparison

--- frontend/src/components/movieNightAttend.tsx
+++ frontend/src/components/movieNightAttend.tsx
@@ -31,7 +31,7 @@ export const MovieNightAttend: FC<MovieDateProps> = ({movieDate, isVisible, setV
     useEffect(() => {
         getMovieNight(movieDate)
         .then((r) => {
-            if (r.length == 0) {
+            if (r.length === 0) {
                 console.log("ni mom wieczorów")
             } else {
                 setNightLocation(r[0].location);
