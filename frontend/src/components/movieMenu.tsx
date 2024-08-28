dd9ff667e11d243f597ce057b3abc88cf1a2c02e
adlbrtbsr <creeper1739@gmail.com>
2024-08-28 19:24:52 +0200

    removed guzik do klikania

--- frontend/src/components/movieMenu.tsx
+++ frontend/src/components/movieMenu.tsx
@@ -4,7 +4,6 @@ import {Avatar} from "primereact/avatar";
 import {clearAccessToken, getAccessToken, getUsername} from "../utils/accessToken.ts";
 import {useNavigate} from "react-router-dom";
 import {ACCOUNT, CALENDAR, HOMEPAGE, LOGIN, MOVIES, NEW_MOVIE} from "../constants/paths.ts";
-import {getRandomMovie} from "../connections/internal/movieNight.ts";
 
 
 export default function MovieMenu() {
@@ -68,13 +67,6 @@ export default function MovieMenu() {
           template: itemRenderer,
           command:() => {
             navigate(CALENDAR)
-          }
-        },
-        {
-          label: 'guzikdoklikania',
-          template: itemRenderer,
-          command:() => {
-            getRandomMovie()
           }
         },
       ]
