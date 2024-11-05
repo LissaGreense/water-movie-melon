dd3da3442ed5f5f86d91e53bc4fab064e60d32cb
adlbrtbsr <creeper1739@gmail.com>
2024-11-06 00:20:22 +0100

    regex

--- frontend/src/components/newMovieForm.tsx
+++ frontend/src/components/newMovieForm.tsx
@@ -17,9 +17,8 @@ export const NewMovieForm = () => {
   const [movieDuration, setMovieDuration] = useState<number>(0);
   const [formIncomplete, setFormIncomplete] = useState<boolean>(true);
   const [posterUrls, setPosterUrls] = useState<string[]>([]);
-  const URL_REGEX: string =
-    "^(https?:\\/\\/)?((([-a-z0-9]{1,63}\\.)*?[a-z0-9]([-a-z0-9]{0,253}[a-z0-9])?\\.[a-z]{2,63})|((\\d{1,3}\\.){3}\\d{1,3}))(:\\d{1,5})?((\\/|\\?)((%[0-9a-f]{2})|[-\\w\\+\\.\\?\\/@~#&=])*)?$";
-  const checkUrl: RegExp = new RegExp(URL_REGEX, "i");
+  const URL_REGEX: RegExp = /^(https?:\/\/)?((([-a-z0-9]{1,63}\.)?a-z0-9?\.[a-z]{2,63})|((\d{1,3}\.){3}\d{1,3}))(:\d{1,5})?(([/?])((%[0-9a-f]{2})|[-\w+.?/@~#&=]))?$/i;
+
 
   const emptyFieldsInForm = useCallback(() => {
     return (
@@ -97,7 +96,7 @@ export const NewMovieForm = () => {
   };
 
   const handleMovieLink = (url: string) => {
-    if (checkUrl.test(url)) {
+    if (URL_REGEX.test(url)) {
       const checkedUrl = checkForHttp(url);
       setMovieLink(checkedUrl);
     } else {
