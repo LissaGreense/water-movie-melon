08a729ee582f6cef9cf019b2ddc858197816305c
adlbrtbsr <creeper1739@gmail.com>
2024-12-12 23:08:51 +0100

    removed option to add movie night when no unwatched movies

--- frontend/src/components/newMovieNightForm.tsx
+++ frontend/src/components/newMovieNightForm.tsx
@@ -1,11 +1,12 @@
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { postMovieNight } from "../connections/internal/movieNight.ts";
-import { Dispatch, FC, SetStateAction, useState } from "react";
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
 import { Calendar } from "primereact/calendar";
 import dayjs from "dayjs";
 import { Dialog } from "primereact/dialog";
 import { getUsername } from "../utils/accessToken.ts";
+import { getMovies } from "../connections/internal/movie.ts";
 
 interface MovieDateProps {
   movieDate: Date | null;
@@ -19,42 +20,62 @@ export const NewMovieNightForm: FC<MovieDateProps> = ({
   setVisible,
 }): JSX.Element => {
   const [nightTime, setNightTime] = useState<Date>();
+  const [availableMovies, setAvailableMovies] = useState<boolean>(false);
 
-  return (
-    <Dialog visible={isVisible} onHide={() => setVisible(false)}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const target = e.currentTarget;
-          postMovieNight(
-            getUsername() as string,
-            dayjs(movieDate).format("YYYY-MM-DD ") +
-              dayjs(nightTime).format("HH:mm"),
-            target.location.value,
-          );
-        }}
-      >
-        <div className="p-inputgroup flex-1 m-2">
-          <span className="p-inputgroup-addon">Data Wieczoru</span>
-          <Calendar value={movieDate} dateFormat={"dd-mm-yy"} disabled />
-          <Calendar
-            value={nightTime}
-            onChange={(e) => setNightTime(e.value as Date | undefined)}
-            timeOnly
-            hourFormat="24"
+  useEffect(() => {
+    getMovies({ watched: false }).then((movies) => {
+      if (movies.length > 0) {
+        setAvailableMovies(true);
+      } else {
+        setAvailableMovies(false);
+      }
+    });
+  }, [isVisible]);
+
+  if (availableMovies) {
+    return (
+      <Dialog visible={isVisible} onHide={() => setVisible(false)}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const target = e.currentTarget;
+            postMovieNight(
+              getUsername() as string,
+              dayjs(movieDate).format("YYYY-MM-DD ") +
+                dayjs(nightTime).format("HH:mm"),
+              target.location.value,
+            );
+          }}
+        >
+          <div className="p-inputgroup flex-1 m-2">
+            <span className="p-inputgroup-addon">Data Wieczoru</span>
+            <Calendar value={movieDate} dateFormat={"dd-mm-yy"} disabled />
+            <Calendar
+              value={nightTime}
+              onChange={(e) => setNightTime(e.value as Date | undefined)}
+              timeOnly
+              hourFormat="24"
+            />
+          </div>
+          <div className="p-inputgroup flex-1 m-2">
+            <span className="p-inputgroup-addon">Lokacja</span>
+            <InputText placeholder="eg. chata starej pepe" name={"location"} />
+          </div>
+          <Button
+            className="m-2"
+            label="Dodaj Wieczór"
+            icon="pi pi-check"
+            type={"submit"}
           />
-        </div>
-        <div className="p-inputgroup flex-1 m-2">
-          <span className="p-inputgroup-addon">Lokacja</span>
-          <InputText placeholder="eg. chata starej pepe" name={"location"} />
-        </div>
-        <Button
-          className="m-2"
-          label="Dodaj Wieczór"
-          icon="pi pi-check"
-          type={"submit"}
-        />
-      </form>
-    </Dialog>
-  );
+        </form>
+      </Dialog>
+    );
+  } else {
+    return (
+      <Dialog visible={isVisible} onHide={() => setVisible(false)}>
+        <span>Nie ma już żadnych filmów do obejrzenia,</span>
+        <span> aby zaplanować wieczór filmowy dodaj jakiś film.</span>
+      </Dialog>
+    );
+  }
 };
